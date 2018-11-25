2d20eaba5fea625ca3c38c222f0d58406bf447b9
illutek <stefanvandenborne@gmail.com>
2018-11-25 21:38:46 +0100

    layout footer-bottom and index.php all includes

---
+++ includes/footer.inc.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by Visuale Studio Code.
+ * User: Stefan
+ * Date: 25/11/2018
+ * Time: 14:48
+ */
+
+?>
+
+<footer class="footer">
+  <div class="footer__wrap container">
+    <div class="footer__content">
+      <div class="footer__one">
+        <a href="#" class="btn btn--blue">Contacteer mij</a>
+      </div>
+      <div class="footer__two">
+        <p>E. B. V. B. A Vasilis Elektriciteit jobs <br>   
+        & Gyproc Afwerking.</p> 
+        <p>BTW  BE 0472 736 428</p> 
+      </div>
+    </div>
+  </div>
+  <div class="container">
+      <div class="footer__bottom-wrap">
+        Copyright © <?php echo date("Y") ?> | Vasilis - <a href="#">Privacy Policy</a> 
+      </div>
+    
+  </div>
+</footer>
---
+++ includes/main-content.inc.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by Visuale Studio Code.
+ * User: Stefan
+ * Date: 25/11/2018
+ * Time: 14:48
+ */
+
+?>
+
+<div class="main container">
+    <div class="main__content">
+      <article class="electricity pnl pnl--blue">
+        <h2>Algemene elektriciteitswerken</h2>
+          <p>Plannen om te bouwen of een woning te renoveren, dan bent u bij mij aan het juiste adres voor uw elektrische installatie. 
+          <p>Dit kan zijn van parlofonie, videofonie, branddetectie, elektrische verwarming, keuringsplannen, verlichting, tuinverlichting, elektrische poorten, telecom installaties, datanetwerken, bekabeling,…
+          </p> 
+        <h3>Herstellingen</h3>
+          <p> Grote of kleine problemen ik probeer je altijd zo snel mogelijk verder te helpen.</p>
+          <p>Voor meer informatie neem gerust <a href="#">contact</a> met mij op.</p>
+      </article>
+
+      <article class="gyproc pnl pnl--red">
+        <h2>Gyproc afwerkingen</h2>
+        <p>Valse wanden en plafonds.</p>
+        <p>Een extra muur of de plaatsing van een nieuw plafond geen probleem deze worden vakkundig geplaatst en schildersklaar afgewerkt.</p>
+        <p>Meer informatie over de opties en mogelijkheden neem gerust <a href="#">contact</a>  met mij op.</p>
+      </article>
+    </div>
+    
+  </div>
